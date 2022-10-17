a2f38238d09cd4e8de92a7421043db84207501c4
Melissa Freitas <melissa.mf29@gmail.com>
2022-10-17 15:15:28 -0300

    doc iac completo

---
+++ Bootcamp-Cloud-DevOps/iac.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Criando Diretorios, users e grupos com scripts
+# Para executar: ./iac.sh
+
+echo "Criando diretorios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuarios..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuarios..."
+
+useradd mei -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+
+useradd ana -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd carol -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+
+useradd marcos -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd joana -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd jose -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+
+echo "Especificando permissoes dos diretorios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Finalizado..."
